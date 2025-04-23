6252491d89922e76c61c25b3101e793d555d6fab
JS <jsteinle1@stud-hs.offenburg.de>
2025-04-23 16:32:46 +0200

    Projekt aufgesetzt

---
+++ v2/jannis/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.example.LogServiceGrpc;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+        LogServiceGrpc.
+    }
+}
