0057554c1a378e1ae53257c07d8af3cc67b440a6
leon <leon.kauk@web.de>
2025-04-23 16:29:18 +0200

    Build erfolgreich

--- v2/leon/build.gradle.kts
+++ v2/leon/build.gradle.kts
@@ -1,19 +1,55 @@
 plugins {
     id("java")
+    id("com.google.protobuf") version "0.9.4"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("io.grpc:grpc-netty-shaded:1.71.0")
+    implementation("io.grpc:grpc-protobuf:1.71.0")
+    implementation("io.grpc:grpc-stub:1.71.0")
+    implementation("javax.annotation:javax.annotation-api:1.3.2")
+
+    testImplementation("junit:junit:4.13.2")
 }
 
-tasks.test {
-    useJUnitPlatform()
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.25.1"
+    }
+    plugins {
+        create("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.71.0"
+        }
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.plugins {
+                create("grpc") {}
+            }
+        }
+    }
 }
+
+tasks.named<ProcessResources>("processResources") {
+    exclude("**/*.proto")
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
+
+sourceSets {
+    main {
+        proto {
+            srcDir("src/main/proto")
+        }
+        java {
+            srcDirs("build/generated/source/proto/main/java", "build/generated/source/proto/main/grpc")
+        }
+    }
+}
+
