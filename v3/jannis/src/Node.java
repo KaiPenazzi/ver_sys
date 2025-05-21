44184bb0ed7cbbabc4e802e2cf9eec87d6e02963
Jannis Steinle <login-29@mailbox.org>
2025-05-21 15:52:46 +0200

    Init Project

---
+++ v3/jannis/src/Node.java
@@ -0,0 +1,11 @@
+public class Node
+{
+    private String ip;
+    private int port;
+
+    private int neigh_informed;
+    private boolean initiator;
+    private boolean informed;
+    private int storage_val;
+    private int sum;
+}
