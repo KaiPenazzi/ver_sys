ef81ff5fa942230a24ea8b48c92f470488da526f
kai <k.penazzi@web.de>
2025-05-21 16:43:33 +0200

    init v3 project

---
+++ v3/kai/node/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    application
+}
+
+application {
+    mainClass.set("com.node.Main")
+}
+
+dependencies {
+    implementation(project(":common"))
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_24
+    targetCompatibility = JavaVersion.VERSION_24
+}
+
