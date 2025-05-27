daa6fef04839643fe06feef6827d725985858df1
JS <jsteinle1@stud-hs.offenburg.de>
2025-05-27 14:56:51 +0200

    Add mynet.json

--- V3/jannis/src/controller/Main.java
+++ V3/jannis/src/controller/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Main
 {
     public static void main(String[] args) throws IOException {
-        Controller controller = new Controller("127.0.0.1:1000", "C:\\Semester 6 Git Repos\\Verteilte Systeme\\ver_sys\\v3\\jannis\\src\\controller\\mynet.json");
+        Controller controller = new Controller("127.0.0.1:1000", "mynet.json");
         controller.getConfigNodes();
     }
 }
